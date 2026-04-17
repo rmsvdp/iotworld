e60828875d197ae79d929422169d816324fa3501
rmsvdp <114291819+rmsvdp@users.noreply.github.com>
2026-04-17 20:15:06 +0200

    Update strip_test.py

--- raspi-pico/strip_test/strip_test.py
+++ raspi-pico/strip_test/strip_test.py
@@ -4,8 +4,9 @@ import time
 from neopixel import Neopixel
 
 numpix = 32
-strip = Neopixel(numpix, 0, 0, "GRB")
-strip.brightness(20)
+strip = Neopixel(numpix, 0, 0, "GRB")  # Ojo a esta codificación
+strip.brightness(20)                   # intensidad en pixels
+# Construir máscara : R, G , B !!
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
